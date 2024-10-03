e4ddae5925e420167994695153d9b0dff16ac847
gauravsroha <gauravsroha05853@gmail.com>
2024-10-03 14:21:19 +0530

    Added functionalities of Add, Update and Delete

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     #path('login/', views.login_user, name = 'login'),
     path('logout/', views.logout_user, name = 'logout'),
     path('register/', views.register_user, name = 'register'),
+    path('record/<int:pk>', views.customer_record, name = 'record'), #<int:pk> means there will be a primary key at the end of url
+    path('delete_record/<int:pk>', views.delete_record, name = 'delete_record'),
+    path('add_record/', views.add_record, name = 'add_record'),
+    path('update_record/<int:pk>', views.update_record, name = 'update_record'),
 ]
